20389f88313102b9baca59cb9b64afdf6ddb67c7
Minh Le <beaver700nh@gmail.com>
2019-07-15 10:23:38 -0400

    Final Commit

--- NumGuessArduino.ino
+++ NumGuessArduino.ino
@@ -8,10 +8,12 @@
 #undef ENTER_KEY
 #undef BKSPC_KEY
 #undef QUITG_KEY
+#undef NEXTB_KEY
 
-const int ENTER_KEY = '#';
-const int BKSPC_KEY = '*';
-const int QUITG_KEY = 'D';
+const int ENTER_KEY = 'A';
+const int BKSPC_KEY = 'B';
+const int QUITG_KEY = 'C';
+const int NEXTB_KEY = 'D';
 
 int realn = 0;
 int input = 0;
@@ -29,12 +31,13 @@ char KEYS[ROWS][COLS] = {
 byte ROWPINS[ROWS] = { 28, 29, 30, 31 };
 byte COLPINS[COLS] = { 32, 33, 34, 35 };
 
-Keypad key(makeKeymap(KEYS), ROWPINS, COLPINS, ROWS, COLS);
+Keypad kp(makeKeymap(KEYS), ROWPINS, COLPINS, ROWS, COLS);
 
 LiquidCrystal lcd(22, 23, 24, 25, 26, 27);
 
 int getGuess(void);
 bool checkGuess(int *realnum, int guess);
+void waitFor(char keyToWait);
 
 void setup(void)
 {
@@ -45,7 +48,7 @@ void setup(void)
   lcd.print("NumGuess");
   lcd.setCursor(0, 1);
   lcd.print("Starting...");
-  delay(1000);
+  waitFor(NEXTB_KEY);
   lcd.clear();
 }
 
@@ -57,9 +60,6 @@ void loop(void)
   {
     lcd.clear();
     lcd.print("Guess num 1-1000");
-
-//    lcd.setCursor(10, 1);
-//    lcd.print(number);
 
     input = getGuess();
 
@@ -75,6 +75,8 @@ void loop(void)
     match = checkGuess(&number, input);
   }
   while (!match);
+
+  waitFor(NEXTB_KEY);
 }
 
 int getGuess(void)
@@ -86,7 +88,7 @@ int getGuess(void)
 
   while (true)
   {
-    guessedKey = key.getKey();
+    guessedKey = kp.getKey();
 
     if (isdigit(guessedKey) && curPos < 4)
     {
@@ -130,35 +132,55 @@ int getGuess(void)
 
 bool checkGuess(int *realnum, int guess)
 {
+  static int tries;
+
   lcd.setCursor(0, 0);
 
   if (*realnum == guess)
   {
     lcd.print("Correct!        ");
-    delay(1500);
+    delay(1250);
+    lcd.setCursor(0, 0);
+
+    char triesStr[16];
+    sprintf(triesStr, "Tries: %-9d", tries);
+
+    lcd.print(triesStr);
+    delay(1250);
 
     *realnum = random(1, 1001);
 
+    tries = 0;
     return true;
   }
   else
   {
+    ++tries;
+
     if (guess < 0 || guess > 1000)
     {
       lcd.print("Out of range.   ");
-      delay(1500);
+      delay(1250);
     }
     if (*realnum < guess)
     {
       lcd.print("Lower!          ");
-      delay(1500);
+      delay(1250);
     }
     if (*realnum > guess)
     {
       lcd.print("Higher!         ");
-      delay(1500);
+      delay(1250);
     }
 
     return false;
+  }
+}
+
+void waitFor(char keyToWait)
+{
+  while (kp.getKey() != keyToWait)
+  {
+    /* wait for keyToWait to be pressed */;
   }
 }
